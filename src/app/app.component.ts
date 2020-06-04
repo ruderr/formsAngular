6fd94515420c66e2567c61c05ea6cd207f3bdce5
ruderr <ruderr@gmail.com>
2020-06-04 00:08:28 -0300

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formularios';
+
+  formulario: FormGroup;
+
+  constructor( private frmBuilder: FormBuilder ) {
+
+    this.formulario = this.frmBuilder.group( {
+      nombre: [ 'nombre por defecto', Validators.required],
+      apellido: [ 'apellido por defecto', Validators.required],
+      email: [ 'prueba@gmail.com', [Validators.required, Validators.pattern("[^ @]*@[^ @]*") ]]
+    });
+
+  }
+
+  onSubmit(parametro){
+
+    console.log(parametro);
+
+  }
 }
